f58228eda7ea1efc84e3addc05253ed8c441d9f9
wjdtnjd <wjdtnjd331@gmail.com>
2024-03-19 18:52:33 +0900

    스캐너 입력받기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.println("== 프로그램 시작 ==");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("명령어)");
+        String cmd = sc.next();
+        System.out.printf("입력된 명령어 : %s\n", cmd);
+
+        System.out.printf("명령어)");
+        int num = sc.nextInt();
+        sc.nextLine();
+        System.out.printf(" 입력된 명령어 : %d\n", num);
+
+        sc.close();
+        System.out.println("== 프로그램 끝 ==");
     }
 }
