bd4503f6d165a777e593fbcdc0a78064895c5e58
wjdtnjd <wjdtnjd331@gmail.com>
2024-03-19 20:35:11 +0900

    3.19

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("== 프로그램 시작 ==");
+        System.out.println("== 프로그램 시작22 ==");
         Scanner sc = new Scanner(System.in);
 
         System.out.printf("명령어)");
@@ -18,6 +18,6 @@ public class Main {
         System.out.printf(" 입력된 명령어 : %d\n", num);
 
         sc.close();
-        System.out.println("== 프로그램 끝 ==");
+        System.out.println("== 프로그램 끝22 ==");
     }
 }
