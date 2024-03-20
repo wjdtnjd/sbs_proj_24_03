d7a97051ae7772625e7a2d9e84be50826f2a7921
wjdtnjd <wjdtnjd331@gmail.com>
2024-03-20 17:41:37 +0900

    게시물 상세 기능 구현, 날짜는 아직

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -52,6 +52,31 @@ public class Main {
                   System.out.printf("%d  : %s\n", article.id, article.title);
               }
           }
+          else if ( cmd.startsWith("article detail ")) {
+              String[] cmdBits = cmd.split(" ");
+              int id = Integer.parseInt(cmdBits[2]);
+
+              Article foundArticle = null;
+
+              for ( int i = 0; i < articles.size(); i++ ) {
+                  Article article = articles.get(i);
+
+                  if ( article.id == id) {
+                      foundArticle = article;
+                      break;
+                  }
+              }
+
+              if ( foundArticle == null ) {
+                  System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+                  continue;
+              }
+              System.out.printf("번호 : %d\n", foundArticle.id);
+              System.out.printf("날짜 : %s\n", "2024-03-20-12:12:12");
+              System.out.printf("제목 : %s\n", foundArticle.title);
+              System.out.printf("내용 : %s\n", foundArticle.body);
+
+          }
           else {
               System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", cmd);
           }
