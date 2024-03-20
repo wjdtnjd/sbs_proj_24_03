8ae00f8fa211776901d8131c663cc157df5d7b1f
wjdtnjd <wjdtnjd331@gmail.com>
2024-03-20 14:30:11 +0900

    게시물 작성기능 구현 중

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,19 +5,42 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("== 프로그램 시작22 ==");
+        System.out.println("== 프로그램 시작 ==");
         Scanner sc = new Scanner(System.in);
 
-        System.out.printf("명령어)");
-        String cmd = sc.next();
-        System.out.printf("입력된 명령어 : %s\n", cmd);
+        int lastArticleId = 0;
 
-        System.out.printf("명령어)");
-        int num = sc.nextInt();
-        sc.nextLine();
-        System.out.printf(" 입력된 명령어 : %d\n", num);
+      while ( true ) {
+          System.out.printf("명령어)");
+          String cmd = sc.nextLine();
+          cmd = cmd.trim();
 
+          if ( cmd.length() == 0) {
+              continue;
+          }
+
+          if ( cmd.equals("exit")) {
+              break;
+          }
+
+          if ( cmd.equals("article write")) {
+              int id = lastArticleId + 1;
+              lastArticleId = id;
+              System.out.printf("제목 : ");
+              String title = sc.nextLine();
+              System.out.printf("내용 : ");
+              String body = sc.nextLine();
+
+              System.out.printf("%d글이 생성되었습니다.\n", id);
+          }
+          else if ( cmd.equals("article list")) {
+              System.out.println("게시물이 없습니다.");
+          }
+          else {
+              System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", cmd);
+          }
+      }
         sc.close();
-        System.out.println("== 프로그램 끝22 ==");
+      System.out.println("== 프로그램 끝 ==");
     }
 }
