d8062e6f1ece0f78428ef448b1c33fe525409d54
wjdtnjd <wjdtnjd331@gmail.com>
2024-03-21 16:34:41 +0900

    특정 게시물을 찾는 메서드 구현 후 중복 제거

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -64,16 +64,8 @@ public class App {
                 String[] cmdBits = cmd.split(" ");
                 int id = Integer.parseInt(cmdBits[2]); // "1" => 1
 
-                Article foundArticle = null;
-
-                for ( int i = 0; i < articles.size(); i++ ) {
-                    Article article = articles.get(i);
-
-                    if ( article.id == id ) {
-                        foundArticle = article;
-                        break;
-                    }
-                }
+                Article foundArticle = getArticleById(id)
+;
 
                 if ( foundArticle == null ) {
                     System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
@@ -92,16 +84,7 @@ public class App {
                 String[] cmdBits = cmd.split(" ");
                 int id = Integer.parseInt(cmdBits[2]); // "1" => 1
 
-                Article foundArticle = null;
-
-                for ( int i = 0; i < articles.size(); i++ ) {
-                    Article article = articles.get(i);
-
-                    if ( article.id == id ) {
-                        foundArticle = article;
-                        break;
-                    }
-                }
+                Article foundArticle = getArticleById(id);
 
                 if ( foundArticle == null ) {
                     System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
@@ -122,16 +105,7 @@ public class App {
                 String[] cmdBits = cmd.split(" ");
                 int id = Integer.parseInt(cmdBits[2]);
 
-                int foundIndex = -1;
-
-                for ( int i = 0; i < articles.size(); i++ ) {
-                    Article article = articles.get(i);
-
-                    if ( article.id == id ) {
-                        foundIndex = i;
-                        break;
-                    }
-                }
+                int foundIndex = getArticleIndexById(id);
 
                 if ( foundIndex == -1 ) {
                     System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
@@ -152,6 +126,29 @@ public class App {
 
         sc.close();
         System.out.println("== 프로그램 끝 ==");
+    }
+
+    private int getArticleIndexById(int id) {
+        int i = 0;
+
+        for ( Article article : articles) {
+            if ( article.id == id) {
+                return i;
+            }
+            i++;
+        }
+
+        return -1;
+    }
+
+    private Article getArticleById(int id) {
+        int index = getArticleIndexById(id);
+
+        if ( index != -1) {
+            return articles.get(index);
+        }
+
+        return null;
     }
 
     private void makeTestData() {
