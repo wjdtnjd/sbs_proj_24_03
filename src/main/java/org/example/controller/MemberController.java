495f1bab77b9b56533ef40dbea31490720da500a
wjdtnjd <wjdtnjd331@gmail.com>
2024-03-22 17:20:54 +0900

    로그인 기능 구현

--- src/main/java/org/example/controller/MemberController.java
+++ src/main/java/org/example/controller/MemberController.java
@@ -12,6 +12,7 @@ public class MemberController extends Controller{
     private List<Member> members;
     private String cmd;
     private String actionMethodName;
+    private Member loginedMember;
 
     public MemberController(Scanner sc) {
         this.sc = sc;
@@ -25,6 +26,11 @@ public class MemberController extends Controller{
         switch ( actionMethodName ) {
             case "join" :
                 doJoin();
+            case "login":
+                doLogin();
+                break;
+            default:
+                System.out.println("존재하지 않는 명령어 입니다.");
                 break;
         }
     }
@@ -71,6 +77,29 @@ public class MemberController extends Controller{
 
         System.out.printf("%d번 회원이 생성되었습니다. 환영합니다!\n", id);
     }
+
+    public void doLogin() {
+        System.out.printf("로그인 아이디 :");
+        String loginId = sc.nextLine();
+        System.out.printf("로그인 비밀번호 :");
+        String loginPw = sc.nextLine();
+
+        Member member = getMemberByLoginId(loginId);
+
+        if ( member == null ) {
+            System.out.println("해당회원은 존재하지 않습니다.");
+            return;
+        }
+
+        if ( member.loginPw.equals(loginPw) == false ) {
+            System.out.println("비밀번호가 일치하지 않습니다.");
+            return;
+        }
+
+        loginedMember = member;
+        System.out.printf("로그인 성공! %s님 환영합니다.\n", loginedMember.name);
+    }
+
     private boolean isJoinableLoginId(String loginId) {
         int index = getMemberIndexByLoginId(loginId);
 
@@ -92,5 +121,14 @@ public class MemberController extends Controller{
         }
 
         return -1;
+    }
+
+    private Member getMemberByLoginId(String loginId) {
+        int index = getMemberIndexByLoginId(loginId);
+
+        if (index == -1) {
+            return null;
+        }
+        return members.get(index);
     }
 }
